ea050d277d548f4366bf049e62db93b5d0c3ff2c
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-14 14:31:34 +0200

    ADD user info prompt and base ticket

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Informazioni utente
+
+const userKm = parseFloat(prompt('Quanti km vuoi percorrere?'))
+const age = parseInt(prompt("Quanti anni hai?"));
+// console.log(userKm)
+// console.log(age)
+
+// Calcolo prezzo base
+
+ const ticketPrice = (userKm * 0.21)
+console.log(ticketPrice)
