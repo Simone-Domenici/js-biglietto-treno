bd4968fc65238cc208ab4c837a733d627c93e837
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-14 17:13:02 +0200

    ADD V2

--- js/app.js
+++ js/app.js
@@ -5,12 +5,34 @@ const age = parseInt(prompt('Quanti anni hai?'));
 // console.log(userKm)
 // console.log(age)
 
-// Calcolo prezzo base
+// // Calcolo prezzo base
 
-const ticketPrice = (userKm * 0.21)
-// console.log(ticketPrice)
+// const ticketPrice = (userKm * 0.21)
+// // console.log(ticketPrice)
 
-// Sconti in base all'età
+// // Sconti in base all'età
+
+// let discount = 0;
+// if (age < 18) {
+//  discount = 0.2; // Sconto del 20%
+// } else if (age >= 65) {
+//   discount = 0.4; // Sconto del 40%
+// }
+// // console.log(age, discount)
+
+// // Prezzo finale con lo sconto
+
+// const finalPrice = ticketPrice - (ticketPrice * discount);
+// // console.log(finalPrice)
+
+// // Prezzo con formato a due cifre decimali
+
+// const formattedPrice = finalPrice.toFixed(2)
+// // console.log(formattedPrice)
+
+// alert(`Il prezzo del tuo biglietto è di ${formattedPrice} \u20ac`)
+
+// V2.0
 
 let discount = 0;
 if (age < 18) {
@@ -18,16 +40,7 @@ if (age < 18) {
 } else if (age >= 65) {
   discount = 0.4; // Sconto del 40%
 }
-// console.log(age, discount)
 
-// Prezzo finale con lo sconto
+const ticketPrice = userKm * 0.21
 
-const finalPrice = ticketPrice - (ticketPrice * discount);
-// console.log(finalPrice)
-
-// Prezzo con formato a due cifre decimali
-
-const formattedPrice = finalPrice.toFixed(2)
-// console.log(formattedPrice)
-
-alert(`Il prezzo del tuo biglietto è di ${formattedPrice} \u20ac`)
+alert(`Il prezzo del tuo biglietto è di ${(ticketPrice - (ticketPrice * discount)).toFixed(2)} \u20ac`);
